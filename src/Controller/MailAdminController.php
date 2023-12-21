4fbd2575fe07e789150d8dd5b01959e1d41be5f6
Clement <clement@webetdesign.com>
2023-12-21 10:12:40 +0100

    Ref: clean code

--- src/Controller/MailAdminController.php
+++ src/Controller/MailAdminController.php
@@ -8,36 +8,12 @@ use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use WebEtDesign\MailerBundle\Entity\Mail;
-use WebEtDesign\MailerBundle\Form\TestForm;
 use WebEtDesign\MailerBundle\Services\EmailBuilder;
-use WebEtDesign\MailerBundle\Transport\TransportChain;
 
 final class MailAdminController extends CRUDController
 {
-    private TransportChain $transportChain;
-
-    public function __construct(TransportChain $transportChain, private EmailBuilder $emailBuilder)
+    public function __construct(private readonly EmailBuilder $emailBuilder)
     {
-        $this->transportChain = $transportChain;
-    }
-
-    public function testAction(Request $request, Mail $mail): Response
-    {
-        $form = $this->createForm(TestForm::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $transport = $this->transportChain->get('twig');
-            if ($transport) {
-                $transport->send($mail, $form->getData()['locale'], null, $form->getData()['email']);
-            }
-        }
-
-        return $this->renderWithExtraParams('@WDMailer/admin/mail/test.html.twig', [
-            'form'   => $form->createView(),
-            'object' => $mail,
-            'action' => 'test',
-        ]);
     }
 
     public function livePreviewAction(Request $request, Mail $mail, string $mode, string $locale): Response
