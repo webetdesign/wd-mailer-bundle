df30646a7456d4a96857a79fc83c7632d1f2005f
Léo STEVENOT <youpidok@gmail.com>
2025-05-05 16:30:09 +0200

    Work ticket

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -7,6 +7,7 @@ enum CategoryEnum: string
     case TRANSACTIONAL_EMAIL    = 'TRANSACTIONAL_EMAIL';
     case PRICE_CAMPAIGN_PRODUCT = 'PRICE_CAMPAIGN_PRODUCT';
     case SALE_ORDER_EMAIL       = 'SALE_ORDER_EMAIL';
+    case WORK_TICKET            = 'WORK_TICKET';
 
     public function label(): string
     {
@@ -14,6 +15,7 @@ enum CategoryEnum: string
             self::TRANSACTIONAL_EMAIL    => 'Email transactionnel',
             self::SALE_ORDER_EMAIL       => 'Email commande de vente',
             self::PRICE_CAMPAIGN_PRODUCT => 'Campagne de prix produits',
+            self::WORK_TICKET            => 'Bons de travail',
         };
     }
 }
