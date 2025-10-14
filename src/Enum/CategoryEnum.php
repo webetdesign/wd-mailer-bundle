46a6d8a64212480e0680b85e392666e6e7f88ad9
Alexis <alexis@webetdesign.com>
2025-10-14 17:44:46 +0200

    Add email handling for contract bid "waiting for answer"
    Introduced a new email event and template for contract bid offers awaiting responses. Updated modal, controller logic, and mail configurations to support the new functionality. Refactored related enums, forms, and DTOs for consistency.

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -4,18 +4,20 @@ namespace WebEtDesign\MailerBundle\Enum;
 
 enum CategoryEnum: string
 {
-    case TRANSACTIONAL_EMAIL    = 'TRANSACTIONAL_EMAIL';
+    case TRANSACTIONAL_EMAIL = 'TRANSACTIONAL_EMAIL';
     case PRICE_CAMPAIGN_PRODUCT = 'PRICE_CAMPAIGN_PRODUCT';
-    case SALE_ORDER_EMAIL       = 'SALE_ORDER_EMAIL';
-    case WORK_TICKET            = 'WORK_TICKET';
+    case SALE_ORDER_EMAIL = 'SALE_ORDER_EMAIL';
+    case WORK_TICKET = 'WORK_TICKET';
+    case CONTRACT_BID = 'CONTRACT_BID';
 
     public function label(): string
     {
         return match ($this) {
-            self::TRANSACTIONAL_EMAIL    => 'Email transactionnel',
-            self::SALE_ORDER_EMAIL       => 'Email commande de vente',
+            self::TRANSACTIONAL_EMAIL => 'Email transactionnel',
+            self::SALE_ORDER_EMAIL => 'Email commande de vente',
             self::PRICE_CAMPAIGN_PRODUCT => 'Campagne de prix produits',
-            self::WORK_TICKET            => 'Bons de travail',
+            self::WORK_TICKET => 'Bons de travail',
+            self::CONTRACT_BID => 'Offre',
         };
     }
 }
