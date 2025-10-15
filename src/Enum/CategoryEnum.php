ba1565a1f5eddc47831b6bf44a801b5b69e07244
Alexis <alexis@webetdesign.com>
2025-10-15 10:30:16 +0200

    Merge detached branch

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -4,19 +4,21 @@ namespace WebEtDesign\MailerBundle\Enum;
 
 enum CategoryEnum: string
 {
-    case TRANSACTIONAL_EMAIL     = 'TRANSACTIONAL_EMAIL';
-    case PRICE_CAMPAIGN_PRODUCT  = 'PRICE_CAMPAIGN_PRODUCT';
-    case SALE_ORDER_EMAIL        = 'SALE_ORDER_EMAIL';
+    case TRANSACTIONAL_EMAIL = 'TRANSACTIONAL_EMAIL';
+    case PRICE_CAMPAIGN_PRODUCT = 'PRICE_CAMPAIGN_PRODUCT';
+    case SALE_ORDER_EMAIL = 'SALE_ORDER_EMAIL';
     case SALE_ORDER_EMAIL_CANCEL = 'SALE_ORDER_EMAIL_CANCEL';
-    case WORK_TICKET             = 'WORK_TICKET';
+    case WORK_TICKET = 'WORK_TICKET';
+    case CONTRACT_BID = 'CONTRACT_BID';
 
     public function label(): string
     {
         return match ($this) {
-            self::TRANSACTIONAL_EMAIL                             => 'Email transactionnel',
+            self::TRANSACTIONAL_EMAIL => 'Email transactionnel',
             self::SALE_ORDER_EMAIL, self::SALE_ORDER_EMAIL_CANCEL => 'Email commande de vente',
-            self::PRICE_CAMPAIGN_PRODUCT                          => 'Campagne de prix produits',
-            self::WORK_TICKET                                     => 'Bons de travail',
+            self::PRICE_CAMPAIGN_PRODUCT => 'Campagne de prix produits',
+            self::WORK_TICKET => 'Bons de travail',
+            self::CONTRACT_BID => 'Offre',
         };
     }
 }
