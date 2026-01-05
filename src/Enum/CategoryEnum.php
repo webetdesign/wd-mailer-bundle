031b0d5425e51c35cce5bf451fadd506c2b3f0ad
Jérôme VALDENAIRE <jerome@webetdesign.com>
2026-01-05 13:55:13 +0100

    category

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -10,15 +10,21 @@ enum CategoryEnum: string
     case SALE_ORDER_EMAIL_CANCEL = 'SALE_ORDER_EMAIL_CANCEL';
     case WORK_TICKET = 'WORK_TICKET';
     case BID = 'BID';
+    case QUOTE = 'QUOTE';
+    case CONTRACT_LINE_PRICING = 'CONTRACT_LINE_PRICING';
+    case INVOICING = 'INVOICING';
 
     public function label(): string
     {
         return match ($this) {
-            self::TRANSACTIONAL_EMAIL => 'Email transactionnel',
+            self::TRANSACTIONAL_EMAIL                             => 'Email transactionnel',
             self::SALE_ORDER_EMAIL, self::SALE_ORDER_EMAIL_CANCEL => 'Email commande de vente',
-            self::PRICE_CAMPAIGN_PRODUCT => 'Campagne de prix produits',
-            self::WORK_TICKET => 'Bons de travail',
-            self::BID => 'Offre',
+            self::PRICE_CAMPAIGN_PRODUCT                          => 'Campagne de prix produits',
+            self::WORK_TICKET                                     => 'Bons de travail',
+            self::INVOICING                                       => 'Facturation',
+            self::BID                                             => 'Offre',
+            self::QUOTE                                           => 'Devis',
+            self::CONTRACT_LINE_PRICING                           => 'Modification des tarifs d\'une ligne de contrat',
         };
     }
 }
